3ebba9ee065b7444bcca6215a3b5ae4bb758aef7
Maha-lakshmi08 <mahalakshmiganeshan.08@gmail.com>
2024-09-24 15:39:14 +0530

    Second Commit

---
+++ myproject/src/Components/classcomp.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export class CLassComp extends React.Component{
+    render(){
+        return <p> This is the Class Component </p>
+    }
+}
+
+
+export class CLassComp1 extends React.Component{
+    render(){
+        return <p> hey! I'm another class </p>
+    }
+}
+
+
+
